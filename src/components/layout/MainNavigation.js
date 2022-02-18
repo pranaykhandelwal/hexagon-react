bd1dd5d2592e60ef4000286dc2258e9f54f93bbd
Sonal <sonal425chouhan@gmail.com>
2022-02-19 00:56:28 +0530

    Added Bootstrap.

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,25 +1,40 @@
 import { NavLink } from 'react-router-dom';
 
-import classes from './MainNavigation.module.css';
+import './MainNavigation.module.css';
+import companyLogo from '../../assets/images/logo.png';
 
 const MainNavigation = () => {
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>Great Quotes</div>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <NavLink to='/quotes' activeClassName={classes.active}>
-              All Quotes
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/new-quote' activeClassName={classes.active}>
-              Add a Quote
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <header>
+    <nav class="navbar navbar-expand-sm navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+            <img src={companyLogo} alt="Logo" style="width:60px;" class=""/> Marketplace
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="create-item.html">Create Item</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="collection.html">Collection</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="list-item.html">List Item</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="profile.html"><i class="far fa-user-circle"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a type="button" class="nav-link btn_wlt" data-bs-toggle="modal" data-bs-target="#wallet_model">
+                        <i class="fas fa-wallet"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     </header>
   );
 };
